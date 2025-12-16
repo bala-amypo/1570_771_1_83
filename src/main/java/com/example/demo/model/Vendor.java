4f1258e2ed8472fb0d86e4d49204379a52047601
bala-amypo <bala@amypo.in>
2025-12-16 10:22:57 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -7,5 +7,28 @@ public class Vendor {
     private String name;
     private String contactEmail;
     private String contactPhone;
-    private
+    private Boolean active;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public Vendor() {}
+
+    public Vendor(String name, String contactEmail, String contactPhone, Boolean active, Timestamp createdAt, Timestamp updatedAt) {
+        this.name = name;
+        this.contactEmail = contactEmail;
+        this.contactPhone = contactPhone;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    pu
 }
