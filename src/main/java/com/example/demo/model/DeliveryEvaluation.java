3282688ab422e0adf2de8beb20bac8f5ec22f2a8
bala-amypo <bala@amypo.in>
2025-12-16 14:24:13 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryEvaluation.java
+++ src/main/java/com/example/demo/model/DeliveryEvaluation.java
@@ -15,6 +15,46 @@ public class DeliveryEvaluation() {
     public DeliveryEvaluation() {}
 
     public DeliveryEvaluation(String vendor, String slaRequirement, Integer actualDeliveryDays, Double qualityScore, Date evaluationDate, Boolean meetsDeliveryTarget, Boolean meetsQualityTarget) {
-        
+        this.vendor = vendor;
+        this.slaRequirement = slaRequirement;
+        this.actualDeliveryDays = actualDeliveryDays;
+        this.qualityScore = qualityScore;
+        this.evaluationDate = evaluationDate;
+        this.meetsDeliveryTarget = meetsDeliveryTarget;
+        this.meetsQualityTarget = meetsQualityTarget;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public String getSlaRequirement() {
+        return slaRequirement;
+    }
+
+    public Integer getActualDeliveryDays() {
+        return actualDeliveryDays;
+    }
+
+    public Double getQualityScore() {
+        return qualityScore;
+    }
+
+    public Date getEvaluationDate() {
+        return evaluationDate;
+    }
+
+    public Boolean getMeetsDeliveryTarget() {
+        return meetsDeliveryTarget;
+    }
+
+    public Boolean getMeetsQualityTarget() {
+        return meetsQualityTarget;
+    }
+
+    public void 
 }
