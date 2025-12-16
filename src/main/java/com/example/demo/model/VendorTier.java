a6c9e7cf6fd50afaa741571d2259367a83a8acd7
bala-amypo <bala@amypo.in>
2025-12-16 15:29:13 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorTier.java
+++ src/main/java/com/example/demo/model/VendorTier.java
@@ -1,5 +1,58 @@
 package com.example.demo.model;
 
 public class VendorTier {
-    private
+    private Long id;
+    private String tierName;
+    private Double minScoreThreshold;
+    private String description;
+    private Boolean active;
+
+    public VendorTier() {}
+
+    public VendorTier(String tierName, Double minScoreThreshold, String description, Boolean active) {
+        this.tierName = tierName;
+        this.minScoreThreshold = minScoreThreshold;
+        this.description = description;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTierName() {
+        return tierName;
+    }
+
+    public Double getMinScoreThreshold() {
+        return minScoreThreshold;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void set() {
+        this. = ;
+    }
+
+    public void set() {
+        this. = ;
+    }
+
+    public void set() {
+        this. = ;
+    }
+
+    public void set() {
+        this. = ;
+    }
 }
