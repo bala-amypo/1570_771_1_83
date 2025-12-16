85ea6dd8e3cc3f68b207d50666820659e2e81eac
bala-amypo <bala@amypo.in>
2025-12-16 15:19:14 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorPerformanceScore.java
+++ src/main/java/com/example/demo/model/VendorPerformanceScore.java
@@ -3,6 +3,29 @@ package com.example.demo.model;
 import java.sql.Timestamp;
 
 public class VendorPerformanceScore {
+    private Long id;
+    private String vendor;
+    private Double onTimePercentage;
+    private Double qualityCompliancePercentage;
+    private Double overallScore;
+    private Timestamp calculatedAt;
 
+    public VendorPerformanceScore() {}
+
+    public VendorPerformanceScore(String vendor, Double onTimePercentage, Double qualityCompliancePercentage, Double overallScore, Timestamp calculatedAt) {
+        this.vendor = vendor;
+        this.onTimePercentage = onTimePercentage;
+        this.qualityCompliancePercentage = qualityCompliancePercentage;
+        this.overallScore = overallScore;
+        this.calculatedAt = calculatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public get() {
+        return ;
+    }
 }
 
