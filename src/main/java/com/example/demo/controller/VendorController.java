f3f13b1961bb2b6c06644a7bd5bcded177ed359b
bala-amypo <bala@amypo.in>
2025-12-17 08:45:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Vendor;
+import com.example.demo.service.VendorService;
+
+@RestController
+@RequestMapping("/api/vendors")
+public class VendorController {
+    @Autowired
+    VendorService vendorService;
+
+    @PostMapping
+    public ResponseEntity<Vendor> createAll(@RequestBody Vendor vendor) {
+        Vendor vnd=vendorService.createVendor(vendor);
+        return ResponseEntity.status(201).body(vnd);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<String> putAll(int id, @RequestBody Vendor vendor) {
+        if(vendorService.updateVendor(id, vendor)!=null) {
+            return ResponseEntity.status(201).body("Successful");
+        }
+        else {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vendor> getById(@PathVariable int id) {
+        Vendor vnd=vendorService.getVendorById(id);
+        return ResponseEntity.status(200).body(vnd);
+    }
+
+    @GetMapping
+    public List<Vendor> getAll() {
+        return vendorService.getAllVendors();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> putAlll(int id, @RequestBody Vendor vendor) {
+        if(vendorService.deactivateVendor(id, vendor)!=null) {
+            return ResponseEntity.status(201).body("Successful");
+        }
+        else {
+            return ResponseEntity.status(404).build();
+        }
+    }
+}
