7fb8ca3a2756cbdae879860a4e251e19caaf4970
bala-amypo <bala@amypo.in>
2025-12-24 14:51:06 +0000

    User commit

--- src/main/java/com/example/demo/service/VendorService.java
+++ src/main/java/com/example/demo/service/VendorService.java
@@ -1,16 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Vendor;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-import com.example.demo.model.Vendor;
-
-@Service
 public interface VendorService {
+
     Vendor createVendor(Vendor vendor);
+
     Vendor updateVendor(Long id, Vendor vendor);
+
     Vendor getVendorById(Long id);
+
     List<Vendor> getAllVendors();
+
     void deactivateVendor(Long id);
 }
