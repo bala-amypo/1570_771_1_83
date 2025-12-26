f9898fe13e890e07e3bcf97fb50e94ac9215c1d6
bala-amypo <bala@amypo.in>
2025-12-26 06:12:00 +0000

    User commit

--- src/main/java/com/example/demo/service/SLARequirementService.java
+++ src/main/java/com/example/demo/service/SLARequirementService.java
@@ -4,9 +4,14 @@ import com.example.demo.model.SLARequirement;
 import java.util.List;
 
 public interface SLARequirementService {
-    SLARequirement createRequirement(SLARequirement req);
-    SLARequirement updateRequirement(Long id, SLARequirement req);
+
+    SLARequirement createRequirement(SLARequirement requirement);
+
+    SLARequirement updateRequirement(Long id, SLARequirement requirement);
+
     SLARequirement getRequirementById(Long id);
+
     List<SLARequirement> getAllRequirements();
+
     void deactivateRequirement(Long id);
 }
